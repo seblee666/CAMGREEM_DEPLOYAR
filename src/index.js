b96f2a9ead12700797a5838ba08e7f712cc4b572
seblee666 <sebastianramos965@gmail.com>
2025-01-28 20:52:31 -0500

    realizar enrutamiento

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {dirname,join} from 'path';
 import {fileURLToPath} from 'url';
+import router from './routes/index.js';
 
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('index'));
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
