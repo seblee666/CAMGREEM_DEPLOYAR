e3721320b47dcae32d9bd898f21ffa89f9fb1df6
seblee666 <sebastianramos965@gmail.com>
2025-01-29 01:56:07 -0500

    Pestañas terminadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-router.get('/', (req, res) => res.render('log', {title: 'CamGreen - Login'}));
-router.get('/dashboard', (req, res) => res.render('dashboard', {title: 'CamGreen - Login'}));
+router.get('/', (req, res) => res.render('log', {title: 'Login'}));
+router.get('/dashboard', (req, res) => res.render('dashboard', {title: 'Dashboard'}));
+router.get('/logros', (req, res) => res.render('logros', {title: 'Logros'}));
+router.get('/perfil', (req, res) => res.render('perfil', {title: 'Perfil'}));
+router.get('/recompensas', (req, res) => res.render('recompensas', {title: 'Recompensas'}));
+router.get('/tareas', (req, res) => res.render('tareas', {title: 'Tareas'}));
 
 export default router;
