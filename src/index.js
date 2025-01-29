6ae708f9c90974278f594ab68e8a38bdc45100b4
seblee666 <sebastianramos965@gmail.com>
2025-01-28 20:19:47 -0500

    .ejs

--- src/index.js
+++ src/index.js
@@ -8,3 +8,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
