9ff6cd0d9af17a1e80e2d6604966be9b6ccf33a1
seblee666 <sebastianramos965@gmail.com>
2025-01-29 01:22:50 -0500

    actualizaciones

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(router);
+app.use(express.static(join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-router.get('/', (req, res) => res.render('index', {title: 'CamGreen - Login'}));
+router.get('/', (req, res) => res.render('log', {title: 'CamGreen - Login'}));
 router.get('/dashboard', (req, res) => res.render('dashboard', {title: 'CamGreen - Login'}));
 
 export default router;
